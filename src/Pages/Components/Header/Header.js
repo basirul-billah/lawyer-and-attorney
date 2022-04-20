87868884b84df1db2fcd40a4e5abf00c73cbaa98
Basirul Billah <basirulbillah@gmail.com>
2022-04-20 19:34:28 +0600

    added responsive navbar

--- src/Pages/Components/Header/Header.js
+++ src/Pages/Components/Header/Header.js
@@ -1,20 +1,23 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <header>
             <>
-                <Navbar bg="dark" variant="dark">
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
                     <Container>
-                        <Navbar.Brand href="#home">Lawyer and Attorney</Navbar.Brand>
-                        <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                            <Nav.Link as={Link} to="#services">Services</Nav.Link>
-                            <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
-                            <Nav.Link as={Link} to="/about">About</Nav.Link>
-                        </Nav>
+                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link as={Link} to="/home">Home</Nav.Link>
+                                <Nav.Link as={Link} to="#services">Services</Nav.Link>
+                                <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+                                <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
                     </Container>
                 </Navbar>
             </>
