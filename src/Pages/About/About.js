6f104e1f1e3a4acc1a5ef48235230d1980805c04
Basirul Billah <basirulbillah@gmail.com>
2022-04-25 04:43:56 +0600

    added content to about

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Card } from 'react-bootstrap';
 
 const About = () => {
     return (
         <div>
-            <h1>About</h1>
+            <Card className='mx-auto my-4' style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>Basirul Billah</Card.Title>
+                    <Card.Text>
+                        I want to build beautiful websites and work as web developer in a good company. I seek to improve myself to an extent where I will get very comfortable using and learning new technology. 
+                    </Card.Text>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -4,7 +4,7 @@ import { Card, Table } from 'react-bootstrap';
 const Blog = () => {
     return (
         <div>
-            <Card className='w-75 mx-auto mt-4'>
+            <Card className='w-50 mx-auto mt-4'>
                 <Card.Header>
                     <Card.Title>Difference between Authentication & Authorization</Card.Title>
                 </Card.Header>
@@ -41,6 +41,30 @@ const Blog = () => {
                                 </tr>
                             </tbody>
                         </Table>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card className='w-50 mx-auto mt-4'>
+                <Card.Header>
+                    <Card.Title>Why are you using firebase? What other options do you have to implement authentication?</Card.Title>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        I use firebase to authenticate user and using that authentication I can create user and perform sign in operation.
+                        <br />
+                        Some of the alternative to firebase authentication are Auth0, MongoDB, Passport, Okta.
+                        <br />
+                        Firebase can also be used to store data and perform operations on data. It can be used as a database for applications. It can be used for cloud messaging service, real time analytics etc.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card className='w-50 mx-auto mt-4 mb-4'>
+                <Card.Header>
+                    <Card.Title>What other services does firebase provide other than authentication</Card.Title>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Text>
+                        Firebase can also be used to store data and perform operations on data. It can be used as a database for applications. It can be used for cloud messaging service, real time analytics etc.
                     </Card.Text>
                 </Card.Body>
             </Card>
